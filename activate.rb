50812544496054b883cefc16abc00ccda1f9e333
Cj-bc <cj-bc@y-modify.org>
2018-11-27 17:39:27 +0900

    Update: v1.1.1

--- activate.rb
+++ activate.rb
@@ -2,9 +2,9 @@ class Activate < Formula
   desc "Activate local work env"
   homepage "https://github.com/Cj-bc/activate.git"
   head "https://github.com/Cj-bc/activate.git"
-  version "1.1.0"
+  version "1.1.1"
   url "https://github.com/Cj-bc/activate/archive/v#{version}.tar.gz"
-  sha256 "df6802e47e28718f316ed810673207b6ec15e19ac8181da64549b947ab3996a6"
+  sha256 "0a9806932dbd91e64c5068c5fba1b5690acdba4260b07fc48504597697e49eeb"
 
   def install
     system "mv", "activate.sh", "activate"
