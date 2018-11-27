ea82a241e3ea78f3721643b4258d2ec8e5a829ea
Cj-bc <cj-bc@y-modify.org>
2018-11-27 17:24:45 +0900

    Add: activate v1.1.0

---
+++ activate.rb
@@ -0,0 +1,19 @@
+class Activate < Formula
+  desc "Activate local work env"
+  homepage "https://github.com/Cj-bc/activate.git"
+  head "https://github.com/Cj-bc/activate.git"
+  url "https://github.com/Cj-bc/activate/archive/v1.0.0.tar.gz"
+  sha256 "df6802e47e28718f316ed810673207b6ec15e19ac8181da64549b947ab3996a6"
+
+  def install
+    system "mv", "activate.sh" "activate"
+    bin.install "activate"
+  end
+
+  def caveats; <<~EOS
+    Please add code below to bashrc in order to activate 'activate':
+    [ -f '#{bin}/activate' ] && . #{bin}/activate
+  EOS
+  end
+
+end
